adb1055a3692adbc1a49c541ea2f9ec5c68dd3ed
Maria Hansen <maria.hansen@computational.bio.uni-giessen.de>
2025-01-10 15:50:46 +0100

    feat: Adding basic example of jinja usage

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 #! usr/bin/env Python3
 
+from jinja2 import Environment, FileSystemLoader
+
 
 def main():
-    pass
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('LandingPage.jinja')
+    with open("renders/index.html", 'w') as output:
+        print(template.render(landingPageOf='BioProfileKit'), file = output)
 
 
 if __name__ == '__main__':
