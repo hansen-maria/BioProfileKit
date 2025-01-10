7215bacf6cbe01869645cd0effad93a8203941a4
Maria Hansen <maria.hansen@computational.bio.uni-giessen.de>
2025-01-10 16:06:13 +0100

    feat: Creating a renders dir if not already existing

--- src/app.py
+++ src/app.py
@@ -4,8 +4,12 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def main():
+    from pathlib import Path
+    Path("renders").mkdir(parents=True, exist_ok=True)
+
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('LandingPage.jinja')
+
     with open("renders/index.html", 'w') as output:
         print(template.render(landingPageOf='BioProfileKit'), file = output)
 
