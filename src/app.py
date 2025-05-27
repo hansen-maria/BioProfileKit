532bcf90884acdafe3f43c13cd04ec1c94d22ab9
Sonja Diedrich <sonja.diedrich@icloud.com>
2025-05-27 15:57:57 +0200

    feat: Added Overview.jinja table

--- src/app.py
+++ src/app.py
@@ -4,21 +4,24 @@ from jinja2 import Environment, FileSystemLoader
 from qc_eda.basic.numerical_data import overview,column_overview
 from utils.file_reader import read_file
 from pathlib import Path
-import pandas as pd
 
 def main():
     file = "../test_data/iedb.tsv"
     df = read_file(file)
-    print(overview(df, file))
-    for i in df.columns:
-        print(column_overview(df, i))
+    general = overview(df, file)
+
+    #for i in df.columns:
+    #    print(column_overview(df, i))
     Path("renders").mkdir(parents=True, exist_ok=True)
 
     env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('LandingPage.jinja')
+    landing_template = env.get_template('LandingPage.jinja')
+    overview_template = env.get_template('Overview.jinja')
+    with open("renders/overview.html", 'w') as output:
+        print(overview_template.render(general=general), file = output)
 
     with open("renders/index.html", 'w') as output:
-        print(template.render(landingPageOf='BioProfileKit'), file = output)
+        print(landing_template.render(landingPageOf='BioProfileKit', file=general.filename), file = output)
 
 
 if __name__ == '__main__':
