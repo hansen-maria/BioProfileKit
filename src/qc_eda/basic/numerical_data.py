74f9b4b58168b60ba26d0a1c613f25199a8e329f
Maria Hansen <maria.hansen@computational.bio.uni-giessen.de>
2025-05-27 13:14:31 +0200

    feat: added plain files for basic analysis

---
+++ src/qc_eda/basic/numerical_data.py
@@ -0,0 +1,13 @@
+"""
+Numerical data:
+- min/max/mean/median-range
+- quartiles
+- mode
+- std
+- sum
+- median absolute deviation
+- coefficient of variation
+- kurtosis
+- skewness
+"""
+
---
+++ src/qc_eda/basic/statistics.py
@@ -0,0 +1,13 @@
+"""
+Statistics:
+- Missing Values
+- Skewness
+- Cardinality
+- Target Leakage
+- Outliers
+- Constant Values
+- Duplicated rows/columns
+- Correlation
+- Imbalance
+- Distribution
+"""
