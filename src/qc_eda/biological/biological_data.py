2c9f24683555137d7d6c4bc3e9324e16b7b6fe97
Sonja Diedrich <sonja.diedrich@icloud.com>
2025-08-23 19:30:26 +0200

    feat: added top 20 barplot dna_rna_columns biological_data.py

--- src/qc_eda/biological/biological_data.py
+++ src/qc_eda/biological/biological_data.py
@@ -7,12 +7,14 @@ Taxonomy: capitalization patterns, potentially invalid names,
 strain info in separate field => Flag for 2 Columns?
 lab measurements; units
 """
-from dataclasses import dataclass
-from typing import List, Dict, Tuple
 from collections import defaultdict, Counter
-from multiprocessing import Pool
+from dataclasses import dataclass
+from itertools import chain
+from typing import List, Dict, Tuple
+
 import numpy as np
 import pandas as pd
+import plotly.express as px
 
 
 @dataclass
@@ -23,6 +25,7 @@ class DNARNAColumns:
     count: List[int]
     nucleotide_count: List[Dict[str, int]]
     k_mers: List[List[Tuple[str, int]]]
+    plot: str | None
 
 
 def count_nmer(sequence, n) -> defaultdict:
@@ -51,12 +54,26 @@ def dna_rna_columns(seqs: pd.Series, k: int = 3, top_n: int = 5) -> DNARNAColumn
     uniques = uniq_tmp[top_idx]
     counts = counts_tmp[top_idx]
 
+    min_len, max_len = min(len(s) for s in uniques), max(len(s) for s in uniques)
+
     gc_count = np.char.count(uniques, 'G') + np.char.count(uniques, 'C')
     lengths = np.char.str_len(uniques).astype(int)
     gc_content = np.round(np.where(lengths > 0, gc_count / lengths * 100, 0.0), 2).tolist()
 
     nucleotide_count = [dict(Counter(seq)) for seq in uniques]
-    k_mers_list = [top_mere(seq, n=k, top=top_n) for seq in uniques]
+    k_mers = [top_mere(seq, n=k, top=top_n) for seq in uniques]
+    barplot = None
+    if min_len == max_len:
+        use_logo = True
+        k = min(6, min_len)
+        print("logo")
+    else:
+        use_logo = False
+        k = 3 if max_len >= 6 else 2
+        # Flache Liste mit chain erstellen
+        flat_kmers = chain.from_iterable(kmers_seq for kmers_seq in k_mers if kmers_seq)
+        kmers, count = zip(*((kmer, np.int64(count)) for kmer, count in flat_kmers))
+        barplot = plot_overview(kmers, count)
 
     return DNARNAColumns(
         sequence=uniques.tolist(),
@@ -64,5 +81,30 @@ def dna_rna_columns(seqs: pd.Series, k: int = 3, top_n: int = 5) -> DNARNAColumn
         length=lengths.tolist(),
         count=counts.tolist(),
         nucleotide_count=nucleotide_count,
-        k_mers=k_mers_list
+        k_mers=k_mers,
+        plot=barplot
     )
+
+
+def plot_overview(kmer, count):
+    fig = px.bar(x=kmer, y=count, height=350, color_discrete_sequence=['#0F65A0'])
+    fig.update_layout(bargap=0.2, plot_bgcolor='white', xaxis_title='K-mers',
+        yaxis_title='Count')
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        tickangle=-45
+
+    )
+    fig.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey'
+    )
+    fig.write_image("test.png")
+    return fig.to_html(full_html=False, include_plotlyjs='cdn')
