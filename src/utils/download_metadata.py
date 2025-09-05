794ee8de231a882a3082753fed2437da58a54bf8
jhahnfeld <84308019+jhahnfeld@users.noreply.github.com>
2025-09-05 19:13:53 +0200

    feat: add cog and tax_id download in download_metadata.py

--- src/utils/download_metadata.py
+++ src/utils/download_metadata.py
@@ -1,30 +1,50 @@
-import shutil
+import io
+import zipfile
 from pathlib import Path
 import pandas as pd
 import requests
 from goatools.obo_parser import GODag
 
-#shutil.unpack_archive(filename, extract_dir)
+def download_file(url: str, file_path: Path, download_text: str):
+    if not file_path.exists():
+        print(f"Begin download of {download_text} file. This may take a while...")
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()  # Fehler abfangen
+            with open(file_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        print("\tDownload completed!")
+    else:
+        print(f"{download_text} file already exists. Skipping download.")
 
-# https://ftp.ncbi.nih.gov/pub/taxonomy/taxcat.zip
-# https://ftp.ncbi.nlm.nih.gov/pub/COG/COG2024/data/cog-24.def.tab
-# https://current.geneontology.org/ontology/go-basic.obo
+
+def download_zip_archive(url: str, zip_file_name: str, file_path: Path, download_text: str):
+    if not file_path.exists():
+        print(f"Begin download of {download_text} archive. This may take a while...")
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            buffer = io.BytesIO()
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    buffer.write(chunk)
+            buffer.seek(0)
+
+            with zipfile.ZipFile(buffer) as zf:
+                with zf.open(zip_file_name) as source_file, open(file_path, "wb") as target:
+                    for chunk in iter(lambda: source_file.read(8192), b""):
+                        target.write(chunk)
+
+        print("\tDownload completed!")
+    else:
+        print(f"{download_text} file already exists. Skipping download.")
+
 
 def get_gene_ontology():
     url = "http://current.geneontology.org/ontology/go-basic.obo"
     obo_file = Path("go-basic.obo")
-    if not obo_file.exists():
-        print("Begin download of Gene Ontologie Metadata file. This may take a while...")
-        response = requests.get(url, stream=True)
-        response.raise_for_status()  # Fehler abfangen
-        with open(obo_file, "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
-        print("Download completed!")
-    else:
-        print("GO Metadata file already exists. Skipping download.")
-    print("Loading GO DAG File...")
+    download_file(url, obo_file, "GO Metadata")
 
+    print("Loading GO DAG File...")
     go_dag = GODag(str(obo_file))
     print(go_dag.version)
     data = list()
@@ -37,4 +57,48 @@ def get_gene_ontology():
 
     print(df)
 
+
+def get_clusters_of_orthologous_groups():
+    url: str = "https://ftp.ncbi.nlm.nih.gov/pub/COG/COG2024/data/cog-24.def.tab"
+    cog_file = Path("cog-24.def.tab")
+    download_file(url, cog_file, "COG Metadata")
+
+    print("Loading COG Tab File...")
+    cog_df = pd.read_csv(
+        cog_file,
+        sep="\t",
+        names=[
+            "COG ID",
+            "COG functional category",
+            "COG name",
+            "Gene name associated with the COG",
+            "Functional pathway associated with the COG",
+            "PubMed ID, associated with the COG",
+            "PDB ID of the structure associated with the COG"
+        ]
+    )
+    print(cog_df)
+
+
+def get_tax_ids():
+    url: str = "https://ftp.ncbi.nih.gov/pub/taxonomy/taxcat.zip"
+    tax_file = Path("categories.dmp")
+    taget_path = Path("./")
+    download_zip_archive(url, str(tax_file), taget_path.joinpath(tax_file), "Tax ID Metadata")
+
+    print("Loading Tax ID File...")
+    tax_df = pd.read_csv(
+        tax_file,
+        sep="\t",
+        names=[
+            "top-level",
+            "species-level",
+            "taxid"
+        ]
+    )
+    print(tax_df)
+
+
 get_gene_ontology()
+get_clusters_of_orthologous_groups()
+get_tax_ids()
